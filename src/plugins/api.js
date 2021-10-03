8f9f4aeedecc369f4587a48846aff8cdc5be6804
Gav Hanna <gavhanna@gmail.com>
2021-10-03 12:20:25 +0100

    Added .env file

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -5,7 +5,7 @@ export default {
     Vue.prototype.$http = axios.create({
       baseURL: "https://api.nasa.gov",
       params: {
-        api_key: "pTLlN30L8ASRyCfLpdxtBqrhnKXLXMJzV0fR1YmE",
+        api_key: process.env.VUE_APP_POTD_API_KEY,
       },
     });
   },
